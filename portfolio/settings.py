11222a145ccca35de79f0a7887c1130030a8bb76
Joy Magdy <joymagdy257@gmail.com>
2026-04-26 22:53:17 +0300

    Fix collectstatic ImproperlyConfigured error

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -92,9 +92,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'projects', 'static'),
-]
+# STATICFILES_DIRS is not needed since projects/static is automatically discovered
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
